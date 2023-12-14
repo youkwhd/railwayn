add6112bed8a08f50f6fb995e162e70f559a81d0
youkwhd <lolywk@tutanota.com>
2023-12-14 19:25:46 +0700

    chore: update data(s)

--- railwayn.cpp
+++ railwayn.cpp
@@ -4,11 +4,16 @@
 #include "trains.h"
 #include "queue.h"
 
-#define STATION_1 "Bungkaih (1)"
+#define STATION_1 "Ketapang (1)"
 #define STATION_2 "Geurugok (2)"
-#define STATION_3 "Krueng Geukueh (3)"
-#define STATION_4 "Krueng Mane (4)"
-#define STATION_5 "Kutablang (5)"
+#define STATION_3 "Pangkajene (3)"
+#define STATION_4 "Patukan (4)"
+#define STATION_5 "Binjai (5)"
+
+#define TRAIN_1 "Harina"
+#define TRAIN_2 "Serayu"
+#define TRAIN_3 "Ambarawa Ekspres"
+#define TRAIN_4 "Tawang Alun"
 
 int main(void)
 {
@@ -23,27 +28,27 @@ int main(void)
     stations_insert_last(stations, STATION_5);
 
     trains_create(trains);
-    trains_insert_last(trains, {"A", 20});
-    trains_insert_last(trains, {"U", 30});
-    trains_insert_last(trains, {"P", 10});
-    trains_insert_last(trains, {"D", 40});
+    trains_insert_last(trains, {TRAIN_1, 20});
+    trains_insert_last(trains, {TRAIN_2, 30});
+    trains_insert_last(trains, {TRAIN_3, 10});
+    trains_insert_last(trains, {TRAIN_4, 40});
 
-    trains_elm_t *trains_a = trains_find(trains, "A");
-    stations_queue_enqueue(trains_a->stations_queue, stations_find(stations, STATION_3));
-    stations_queue_enqueue(trains_a->stations_queue, stations_find(stations, STATION_4));
+    trains_elm_t *train_1 = trains_find(trains, TRAIN_1);
+    stations_queue_enqueue(train_1->stations_queue, stations_find(stations, STATION_3));
+    stations_queue_enqueue(train_1->stations_queue, stations_find(stations, STATION_4));
 
-    trains_elm_t *trains_u = trains_find(trains, "U");
-    stations_queue_enqueue(trains_u->stations_queue, stations_find(stations, STATION_1));
-    stations_queue_enqueue(trains_u->stations_queue, stations_find(stations, STATION_4));
-    stations_queue_enqueue(trains_u->stations_queue, stations_find(stations, STATION_2));
+    trains_elm_t *train_2 = trains_find(trains, TRAIN_2);
+    stations_queue_enqueue(train_2->stations_queue, stations_find(stations, STATION_1));
+    stations_queue_enqueue(train_2->stations_queue, stations_find(stations, STATION_4));
+    stations_queue_enqueue(train_2->stations_queue, stations_find(stations, STATION_2));
 
-    trains_elm_t *trains_p = trains_find(trains, "P");
-    stations_queue_enqueue(trains_p->stations_queue, stations_find(stations, STATION_4));
+    trains_elm_t *train_3 = trains_find(trains, TRAIN_3);
+    stations_queue_enqueue(train_3->stations_queue, stations_find(stations, STATION_4));
 
-    trains_elm_t *trains_d = trains_find(trains, "D");
-    stations_queue_enqueue(trains_d->stations_queue, stations_find(stations, STATION_2));
-    stations_queue_enqueue(trains_d->stations_queue, stations_find(stations, STATION_3));
-    stations_queue_enqueue(trains_d->stations_queue, stations_find(stations, STATION_1));
+    trains_elm_t *train_4 = trains_find(trains, TRAIN_4);
+    stations_queue_enqueue(train_4->stations_queue, stations_find(stations, STATION_2));
+    stations_queue_enqueue(train_4->stations_queue, stations_find(stations, STATION_3));
+    stations_queue_enqueue(train_4->stations_queue, stations_find(stations, STATION_1));
     trains_debug(trains);
 
     trains_cleanup(trains);
