fbd5fc920dce5b1cfa6892072a301c156492dfbe
pekoy <mhmmdfaiz160@gmail.com>
2023-12-16 13:40:16 +0700

    add: delete trains

--- trains.h
+++ trains.h
@@ -41,10 +41,11 @@ trains_elm_t *trains_find(trains_t &tr, std::string name);
 void trains_cleanup(trains_t &tr);
 /* 3. 2 */
 /* 3. 8 */
-void trains_debug(trains_t &tr, bool show_queue);
+void trains_debug(trains_t &tr, bool show_ticket);
 /* 3. 9 */
 stations_elm_t *trains_find_station_dest(trains_elm_t *train, std::string station_name);
 /* 3. 10 */
 void __trains_delete_station_entirely(trains_t &tr, stations_t &st, trains_elm_t *train, std::string station_name);
 bool trains_has_tickets(trains_t &tr);
 void trains_simulate_run(trains_t &tr, stations_t &st);
+trains_infotype_t trains_delete(trains_t &tr, trains_elm_t *train);
